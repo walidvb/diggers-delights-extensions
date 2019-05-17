aafbdd0b17673c7f7db5a63747a548463ee95bfd
Walid <hello@walidvb.com>
2019-05-17 10:41:45 -0300

    popup in window, duh

--- iframe.js
+++ iframe.js
@@ -1,27 +1,6 @@
 
 function openIframe() {
-  var div = document.createElement('div');
-  var overflow = document.body.style.overflow;
-  document.body.style.overflow = 'hidden';
-  window.DDCloseIframe = function () {
-    div.remove();
-    document.body.style.overflow = overflow;
-    isFrameOpen = false
-  };
-  div.style.cssText = 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0,0,0,.5); display: flex; align-items: center; justify-content:center;flex-direction: column;z-index:10000; font-size: 16px;';
-  var iframe = document.createElement('iframe');
-  var iframeContainer = document.createElement('div');
-  iframeContainer.style.position = 'relative';
-  var close = document.createElement('div');
-  close.innerText = '╳';
-  close.style.cssText = 'color: white;position: absolute;cursor: pointer;right: 15px;top: 15px;';
-  iframeContainer.append(close);
-  close.addEventListener('click', DDCloseIframe);
-  iframe.frameBorder = 'none';
-  iframe.style.cssText = 'width: 900px; max-width: 90vw; height: 80vh; z-index: 10000';
-  iframe.src = '//www.diggersdelights.net/tracks/new?modal=true&version=0.1&url=' + encodeURIComponent(window.location);
-  iframeContainer.append(iframe);
-  div.append(iframeContainer);
-  document.body.append(div);
+  var src = 'http://www.diggersdelights.net/tracks/new?modal=true&version=0.1&url=' + encodeURIComponent(window.location);
+  window.open(src, 'shareWindow', 'height=700, width=850, top=' + Math.max(window.outerHeight / 2 - 325, 0) + ', left=' + Math.max(window.outerWidth / 2 - 425, 0) + ', toolbar=0, location=0, menubar=no, directories=0, scrollbars=0');
 }
 openIframe();
