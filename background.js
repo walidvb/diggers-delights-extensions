408fab96a39abfdd6bccee7d94b5b05d7937ee28
Walid <hello@walidvb.com>
2019-09-02 17:26:17 +0200

    adds empty clique message

--- background.js
+++ background.js
@@ -1,9 +1,23 @@
 chrome.browserAction.onClicked.addListener(showIFrame);
 function showIFrame() {
-  chrome.tabs.executeScript(null, { file: "iframe.js" });
+  chrome.tabs.executeScript(null, { 
+    file: "dig.js", 
+  });
 }
 
 chrome.contextMenus.create({
   title: 'Dig this!',
-  onclick: showIFrame,
+  onclick: function() {
+    chrome.tabs.executeScript(null, {
+      file: "dig.js",
+    });
+  },
+})
+chrome.contextMenus.create({
+  title: 'Open as a playlist',
+  onclick: function() {
+    chrome.tabs.executeScript(null, {
+      file: "curated.js",
+    });
+  },
 })
--- dig.js
+++ dig.js
@@ -1,4 +1,3 @@
-
 function openIframe() {
   var src = 'http://www.diggersdelights.net/tracks/new?modal=true&version=0.1&url=' + encodeURIComponent(window.location);
   window.open(src, 'shareWindow', 'height=700, width=850, top=' + Math.max(window.outerHeight / 2 - 325, 0) + ', left=' + Math.max(window.outerWidth / 2 - 425, 0) + ', toolbar=0, location=0, menubar=no, directories=0, scrollbars=0');
