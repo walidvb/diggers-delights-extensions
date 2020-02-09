b06df5c11ff99fe6862491fb0a34ca58aca8012c
Walid <hello@walidvb.com>
2020-02-08 19:03:07 -0500

    working like toggler

--- content.js
+++ content.js
@@ -1,59 +1,70 @@
-// const URL = 'http://www.diggersdelights.net'
-const URL = 'http://localhost:3000'
+(() => {
+  // const URL = 'http://www.diggersdelights.net'
+  const URL = 'http://localhost:3000'
 
 
-const wrapper = document.createElement('div')
-wrapper.id = 'diggers-delights-on-page'
-document.body.append(wrapper)
+  const wrapper = document.createElement('div')
+  wrapper.id = 'diggers-delights-on-page'
+  document.body.append(wrapper)
 
-addLikeTrigger()
-async function addLikeTrigger(){
-  const btn = document.createElement('div')
-  btn.innerText = '♥'
-  wrapper.append(btn)
-  btn.classList.add('like')
-  const getCurrentStatus = async () => {
-    const res = await fetch(`${URL}/api/links/like/status?url=${encodeURIComponent(window.location)}`)
-    if(res.status >= 200 && res.status < 300){
-      const { status } = await res.json()
-      if(status){
-        btn.classList.add('liked')
+  addLikeTrigger()
+  async function addLikeTrigger(){
+    const btn = document.createElement('div')
+    btn.innerText = '♥'
+    wrapper.append(btn)
+    btn.classList.add('like')
+    const getCurrentStatus = async () => {
+      const res = await fetch(`${URL}/api/links/like/status?url=${encodeURIComponent(getCanonicalURL())}`)
+      if(res.status >= 200 && res.status < 300){
+        const { liked } = await res.json()
+        if (liked){
+          btn.classList.add('liked')
+        }
+      }
+    }
+    getCurrentStatus()
+    btn.onclick = async () => {
+      fetch
+      const res = await fetch(`${URL}/api/links/like/toggle`, {
+        method: 'POST',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: getCanonicalURL(),
+        })
+      })
+      if (res.status >= 200 && res.status < 300) {
+        const { liked } = await res.json()
+        btn.classList.toggle('liked', liked)
       }
     }
   }
-  getCurrentStatus()
-  btn.onclick = async () => {
-    fetch
-    const res = await fetch(`${URL}/api/links/like`, {
-      method: 'POST',
-      headers: { 
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        url: window.location
-      })
-    })
-    if (res.status >= 200 && res.status < 300) {
-      const { status } = await res.json()
-      btn.classList.toggle('liked', status)
+
+
+  addDialogTrigger()
+  function addDialogTrigger(){
+    const span = document.createElement('span')
+    span.classList.add('dialog-trigger')
+    span.innerText = "Dig This!"
+    wrapper.append(span)
+    if(/soundcloud|mixcloud|nts/.test(window.location)){
+      wrapper.classList.add('higher')
+    }
+    span.onclick = () => {
+      const src = `${URL}/tracks/new?modal=true&version=0.1&url=${encodeURIComponent(getCanonicalURL())}`;
+      window.open(src, 'shareWindow', 'height=700, width=850, top=' + Math.max(window.outerHeight / 2 - 325, 0) + ', left=' + Math.max(window.outerWidth / 2 - 425, 0) + ', toolbar=0, location=0, menubar=no, directories=0, scrollbars=0');
     }
   }
-}
 
 
-addDialogTrigger()
-function addDialogTrigger(){
-  const span = document.createElement('span')
-  span.classList.add('dialog-trigger')
-  span.innerText = "Dig This!"
-  wrapper.append(span)
-  if(/soundcloud|mixcloud|nts/.test(window.location)){
-    wrapper.classList.add('higher')
+  function getCanonicalURL(){
+    const canon = document.querySelector('link[rel="canonical"]')
+    if (canon){
+      canon.attributes.href
+    }
+    return window.location.href
+    
   }
-  span.onclick = () => {
-    const src = `${URL}/tracks/new?modal=true&version=0.1&url=${encodeURIComponent(window.location)}`;
-    window.open(src, 'shareWindow', 'height=700, width=850, top=' + Math.max(window.outerHeight / 2 - 325, 0) + ', left=' + Math.max(window.outerWidth / 2 - 425, 0) + ', toolbar=0, location=0, menubar=no, directories=0, scrollbars=0');
-  }
-}
-
-console.log('Thank you for using diggersdelights!')
+  console.log('Thank you for using diggersdelights!')
+})()
