1708749a74cbf6740f92d75f31eecdd2d2b7cf87
Walid <hello@walidvb.com>
2020-03-08 22:28:51 -0300

    load and execute from server code

--- background.js
+++ background.js
@@ -14,10 +14,42 @@ chrome.contextMenus.create({
   },
 })
 chrome.contextMenus.create({
-  title: 'Open as a playlist',
+  title: 'Listen in one go',
   onclick: function() {
     chrome.tabs.executeScript(null, {
       file: "curated.js",
     });
   },
 })
+chrome.contextMenus.create({
+  title: 'Like',
+  onclick: function() {
+    var someVar = { text: 'test', foo: 1, bar: false };
+    chrome.tabs.executeScript({
+      code: '(' + function (params) {
+        document.body.insertAdjacentHTML('beforeend',
+        '<div style="all:unset; position:fixed; left:0; top:0; right:0; bottom:0;' +
+        'background-color:rgba(0,255,0,0.3)">' + params.text + '</div>'
+        );
+        return { success: true, html: document.body.innerHTML };
+      } + ')(' + JSON.stringify(someVar) + ');'
+    }, function (results) {
+      console.log(results[0]);
+    });
+  },
+})
+
+
+// var someVar = { text: 'test', foo: 1, bar: false };
+// chrome.tabs.executeScript({
+//   code: '(' + function (params) {
+//     document.body.insertAdjacentHTML('beforeend',
+//       '<div style="all:unset; position:fixed; left:0; top:0; right:0; bottom:0;' +
+//       'background-color:rgba(0,255,0,0.3)">' + params.text + '</div>'
+//     );
+//     return { success: true, html: document.body.innerHTML };
+//   } + ')(' + JSON.stringify(someVar) + ');'
+// });
+
+
+console.log('asdasfas')
--- content.js
+++ content.js
@@ -1,70 +1,138 @@
-(() => {
-  // const URL = 'http://www.diggersdelights.net'
-  const URL = 'http://localhost:3000'
-
-
-  const wrapper = document.createElement('div')
-  wrapper.id = 'diggers-delights-on-page'
-  document.body.append(wrapper)
-
-  addLikeTrigger()
-  async function addLikeTrigger(){
-    const btn = document.createElement('div')
-    btn.innerText = '♥'
-    wrapper.append(btn)
-    btn.classList.add('like')
-    const getCurrentStatus = async () => {
-      const res = await fetch(`${URL}/api/links/like/status?url=${encodeURIComponent(getCanonicalURL())}`)
-      if(res.status >= 200 && res.status < 300){
-        const { liked } = await res.json()
-        if (liked){
-          btn.classList.add('liked')
-        }
-      }
-    }
-    getCurrentStatus()
-    btn.onclick = async () => {
-      fetch
-      const res = await fetch(`${URL}/api/links/like/toggle`, {
-        method: 'POST',
-        headers: { 
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          url: getCanonicalURL(),
-        })
-      })
-      if (res.status >= 200 && res.status < 300) {
-        const { liked } = await res.json()
-        btn.classList.toggle('liked', liked)
-      }
-    }
+var scriptURL = 'http://localhost:3001/extension.js';
+var xhr = new XMLHttpRequest();
+xhr.open("GET", scriptURL, true);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState == 4) {
+    console.log('xhr.responseText', xhr.responseText)
+    debugger
+    // WARNING! Might be evaluating an evil script!
+    var resp = eval("(" + xhr.responseText + ")");
+    // Or this if it's work
   }
+}
+xhr.send();
+
+// (() => {
+//   alert("content")
+//   const URL = '//diggersdelights.herokuapp.com'
+//   const fetchInit = {
+//     mode: 'cors',
+//     credentials: 'include'
+//   }
+
+//   const wrapper = document.createElement('div')
+//   wrapper.id = 'diggers-delights-on-page'
+//   document.body.append(wrapper)
 
 
-  addDialogTrigger()
-  function addDialogTrigger(){
-    const span = document.createElement('span')
-    span.classList.add('dialog-trigger')
-    span.innerText = "Dig This!"
-    wrapper.append(span)
-    if(/soundcloud|mixcloud|nts/.test(window.location)){
-      wrapper.classList.add('higher')
-    }
-    span.onclick = () => {
-      const src = `${URL}/tracks/new?modal=true&version=0.1&url=${encodeURIComponent(getCanonicalURL())}`;
-      window.open(src, 'shareWindow', 'height=700, width=850, top=' + Math.max(window.outerHeight / 2 - 325, 0) + ', left=' + Math.max(window.outerWidth / 2 - 425, 0) + ', toolbar=0, location=0, menubar=no, directories=0, scrollbars=0');
-    }
-  }
+
+//   init()
+//   async function init(){ 
+//     if(await isLoggedIn()){
+//       setupUI()
+//     }
+//     else{
+//       setupSignIn()
+//     }
+//   }
+
+//   async function isLoggedIn(){
+//     return true
+//     try{
+//       const { status } = await fetch(`${URL}/api/me.json`, fetchInit)
+//       return status >= 200 && status < 300
+//     }catch(err){
+//       console.error(err)
+//     }
+//     return false
+//   }
+
+//   function setupSignIn(){
+//     const btn = document.createElement('div')
+//     btn.innerText = 'Sign In'
+//     btn.classList.add('dialog-trigger')
+//     wrapper.append(btn)
+//     btn.onclick = () => {
+//       const src = `${URL}/login?modal=true&version=0.1&url=${encodeURIComponent(getCanonicalURL())}`;
+//       openModal(src)
+//     }
+//   }
+//   function setupUI(){
+//     addLikeTrigger()
+//     addDialogTrigger()
+//     async function addLikeTrigger(){
+//       const btn = document.createElement('div')
+//       btn.innerText = '♥'
+//       wrapper.append(btn)
+//       btn.classList.add('like')
+//       const getCurrentStatus = async () => {
+//         const res = await fetch(`${URL}/api/links/like/status?url=${encodeURIComponent(getCanonicalURL())}`)
+//         if(res.status >= 200 && res.status < 300){
+//           const { liked } = await res.json()
+//           if (liked){
+//             btn.classList.add('liked')
+//           }
+//         }
+//       }
+//       getCurrentStatus()
+//       btn.onclick = async () => {
+//         fetch
+//         const res = await fetch(`${URL}/api/links/like/toggle`, {
+//           method: 'POST',
+//           headers: { 
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify({
+//             url: getCanonicalURL(),
+//           })
+//         })
+//         if (res.status >= 200 && res.status < 300) {
+//           const { liked } = await res.json()
+//           btn.classList.toggle('liked', liked)
+//         }
+//       }
+//     }
+//     function addDialogTrigger(){
+//       const span = document.createElement('span')
+//       span.classList.add('dialog-trigger')
+//       span.innerText = "Dig This!"
+//       wrapper.append(span)
+//       if(/soundcloud|mixcloud|nts/.test(window.location)){
+//         wrapper.classList.add('higher')
+//       }
+//       span.onclick = () => {
+//         const src = `${URL}/tracks/new?modal=true&version=0.1&url=${encodeURIComponent(getCanonicalURL())}`;
+//         openModal(src)
+//       }
+//     }
+//   }
 
 
-  function getCanonicalURL(){
-    const canon = document.querySelector('link[rel="canonical"]')
-    if (canon){
-      canon.attributes.href
-    }
-    return window.location.href
-    
-  }
-  console.log('Thank you for using diggersdelights!')
-})()
+//   function getCanonicalURL(){
+//     const canon = document.querySelector('link[rel="canonical"]')
+//     if (canon){
+//       canon.attributes.href
+//     }
+//     return window.location.href 
+//   }
+
+//   function openModal(src){
+//     window.open(src, 'shareWindow', 'height=700, width=850, top=' + Math.max(window.outerHeight / 2 - 325, 0) + ', left=' + Math.max(window.outerWidth / 2 - 425, 0) + ', toolbar=0, location=0, menubar=no, directories=0, scrollbars=0');
+//   }
+//   console.log('Thank you for using diggersdelights!')
+// })()
+
+
+// chrome.tabs.executeScript(tab.id, {
+//   code: 'var script=document.createElement(\'script\');' +
+//     'script.onload=function() { alert("loaded") };' +
+//     'script.src=\'' + scriptURL + '\';' +
+//     'document.body.appendChild(script);'
+// }, function (returnedValue) {
+//   // do something in the extension context after the code was executed
+// });
+
+// var script=document.createElement('script')
+// script.onload=function() { alert("loaded") };
+// script.src= scriptURL ;
+// document.body.appendChild(script);
